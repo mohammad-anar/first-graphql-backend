032a2b7466b195185d97ecc93bdb246ad274b924
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-06-07 02:01:37 +0600

    prisma postgresql configured

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@as-integrations/express5";
+import { prisma } from "./lib/db.js";
 
 const init = async () => {
-
-    const port = Number(process.env.PORT) || 5000;
+  const port = Number(process.env.PORT) || 5000;
   const app = express();
 
   app.use(express.json());
@@ -16,17 +16,43 @@ const init = async () => {
             hello: String
             say(name: String): String
         }
+
+        type Mutation {
+          createUser(firstName: String!, lastName: String!, email: String!, password: String!, profileImageURL: String): Boolean
+        }
     `,
     resolvers: {
-        Query: {
-            hello: () => `Hey there, I'm graphql server!`,
-            say: (_, {name}: {name: string}) => `Hey ${name}, How are you?`
-        }
+      Query: {
+        hello: () => `Hey there, I'm graphql server!`,
+        say: (_, { name }: { name: string }) => `Hey ${name}, How are you?`,
+      },
+
+      Mutation: {
+        createUser: async (
+          _,
+          { firstName, lastName, email, password, profileImageURL },
+        ) => {
+          try {
+            await prisma.user.create({
+              data: {
+                firstName,
+                lastName,
+                email,
+                password,
+                profileImageURL,
+              },
+            });
+            return true;
+          } catch (error) {
+            console.error("Error creating user:", error);
+            return false;
+          }
+        },
+      },
     },
   });
 
   await gqlServer.start();
-
 
   app.get("/", (req, res) =>
     res.json({ message: `Apollo graphql server is running` }),
@@ -37,7 +63,6 @@ const init = async () => {
   app.listen(port, () => {
     console.log(`Apollo server is running on port ${port}`);
   });
-
 };
 
 init();
